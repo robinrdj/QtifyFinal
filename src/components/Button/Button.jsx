914b9339349a36a7c6b171dfbfd5dc786d5f088b
robinrdj <robinrajaduraij@gmail.com>
2024-12-22 22:30:24 +0530

    changer here

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-function Button({content}) {
-  return <div className={styles.button}>
-    {content}
-    </div>;
+function Button({ content }) {
+  return <button className={styles.button}>{content}</button>;
 }
 
 export default Button;
